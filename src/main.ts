65e4627b6c4fa0ad1ff29fa9ebe6b07292d07497
Timur <timengalytchev@mail.ru>
2024-06-19 13:17:45 +0300

    .env test

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './bot/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: 'https://incredible-marzipan-6b9559.netlify.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept', 
+  })
   await app.listen(3000);
 }
 bootstrap();
